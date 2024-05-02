0b79c0df567acf70ea9ec7af8d4f19505a57ca68
Salahsalhi76 <salaheddinesalhi4@gmail.com>
2024-05-02 10:13:14 +0100

    re-arrange middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
-        health: '/up',
-    )
+
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(Cors::class);
 
@@ -23,6 +19,11 @@ return Application::configure(basePath: dirname(__DIR__))
             'guest'=>Guest::class
         ]);
     })
+    ->withRouting(
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up',
+    )
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
